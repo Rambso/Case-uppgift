1a54d5cccb5582bd8f354709b440cf3b9d24760d
Lowe <Lowe.goth@gmail.com>
2025-01-04 15:10:22 +0100

    System info and file management

---
+++ computer_info.bash
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+echo "Compuer name: $(hostname)" 
+echo "OS Description: $(uname -a | cut -d ' ' -f 3)"
+echo "Linux kernal: $(uname -r)"
+echo "CPU: $(lscpu | tr -s ' ' | grep name | cut -d ' ' -f 3-8)"
+echo "Total memory: $(free -h | tr -s ' ' | grep Mem | cut -d ' ' -f 4)"
+echo "Free disk space: $(df -h | tr -s ' ' | sed -n 3p | cut -d ' ' -f 4-5)"
+echo "IP-address: $(ip addr | grep inet | sed -n 3p | tr -s ' ' | cut -d ' ' -f 3 | cut -d / -f 1)"
